cc078653afa76636ee8492f65d1e7fc69f8f8112
jhiver <jhiver@localhost.localdomain>
2018-11-18 14:34:41 +0400

    fixed tal:attributes on null, undefined or false expression result

--- tests/07-tal-attributes.js
+++ tests/07-tal-attributes.js
@@ -12,6 +12,12 @@ talAttributes2 = `
 </html>
 `
 
+talAttributes3 = `
+<html>
+    <body tal:attributes="something foo">Dummy Content</body>
+</html>
+`
+
 describe('attributesData', function() {
   return it('should work', function(done) {
     tal.process(talAttributes, {foo: 'FOO', bar: 'BAR'}, function (error, result){
@@ -52,6 +58,37 @@ describe('attributesAsyncFunction', function() {
       if (error) return done(error)
       if (!String(result).match('something="FOO"')) return done("FOO not there")
       if (!String(result).match('somethingelse="BAR"')) return done("BAR not there")
+      return done()
+    })
+  })
+})
+
+
+describe('attributesRemove', function() {
+  return it('should remove attributes on undefined', function(done) {
+    tal.process(talAttributes3, {foo: undefined}, function (error, result){
+      if (error) return done(error)
+      if (String(result).match('something')) return done("something is there")
+      return done()
+    })
+  })
+})
+
+describe('attributesRemove2', function() {
+  return it('should remove attributes on undefined', function(done) {
+    tal.process(talAttributes3, {foo: null}, function (error, result){
+      if (error) return done(error)
+      if (String(result).match('something')) return done("something is there")
+      return done()
+    })
+  })
+})
+
+describe('attributesRemove3', function() {
+  return it('should remove attributes on undefined', function(done) {
+    tal.process(talAttributes3, {foo: false}, function (error, result){
+      if (error) return done(error)
+      if (String(result).match('something')) return done("something is there")
       return done()
     })
   })
